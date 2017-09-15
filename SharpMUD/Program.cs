fa1bf9572cc6b01f46e635b51190569d7cc09089
Baala <Baala@MAMMON>
2017-09-15 00:41:22 -0400

    initial

---
+++ SharpMUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpMUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
